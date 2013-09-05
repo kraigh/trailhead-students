0eb062e5fc2f007267bf039673fb5301ccf69347
Kraig Hufstedler <kraigory@gmail.com>
2013-09-05 10:06:40 -0500

    fixes to focuser for panels

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,35 @@
 (function ($) {$(document).ready(function() {
 
+
+  // SET THE DEFAULT SCROLL OFFSET HERE
+  var offsetDefault = 0;
+
+
   $(document).on('click', '.focus', function(){
     var id = $(this).attr('data-target-id');
-    // Let's unbind the scroll function now, to account for nav clicks while a panel is in focus.
-    $(window).unbind();
-    // Initialize scrolled variable.
-    var scrolled = 0;
+    // Check for the scroll class and set the variable accordingly.
+    if $(this).hasClass('scroll') {
+      var scroll = true;
+    } else {
+      var scroll = false;
+    }
+    // Check for an offset option. If none, set a default.
+    if ($(this).attr('data-scroll-offset') && $.isNumeric($(this).attr('data-scroll-offset'))) {
+      var offset = $(this).attr('data-scroll-offset');
+    } else {
+      // If you would like, you can change this value to adjust the default offset.
+      var offset = offsetDefault;
+    }
+    focusOn(id, scroll, offset);
+  });
+});})(jQuery);
+
+function focusOn(id, scroll, offset) {
+  // Let's unbind the scroll function now, to account for nav clicks while a panel is in focus.
+  $(window).unbind();
+  // Initialize scrolled variable.
+  var scrolled = 0;
+  if (scroll == true) {
     // Start scrollin'
     $.smoothScroll({
       scrollElement: null,
@@ -22,31 +46,38 @@
         });
       }
     });
-    // Set all panel divs to default style, in case one is currently in focus
-    $('div.panel').css({
-      'z-index': '0',
-      'position': 'static',
-      '-webkit-box-shadow': 'none',
-      '-moz-box-shadow': 'none',
-      'box-shadow': 'none'
+  } else {
+    $(window).bind("scroll", function() {
+      scrolled++;
+      if (scrolled > 10) {
+        closeBg(id);
+        $(window).unbind();
+        scrolled = 0;
+      }
     });
-    // Bring the desired panel div into focus
-    $('div.panel#' + id).css({
-      'z-index': '999',
-      'position': 'relative',
-      '-webkit-box-shadow': '0px 0px 30px rgba(0, 0, 0, 0.32)',
-      '-moz-box-shadow': '0px 0px 30px rgba(0, 0, 0, 0.32)',
-      'box-shadow': '0px 0px 30px rgba(0, 0, 0, 0.32)'
-    });
-    // RELEASE THE UNDERLAY
-    if (!$('#modal-underlay').length) {
-      $('body').append('<div id="modal-underlay" onclick="closeBg(\''+id+'\');"></div>');
-      $('#modal-underlay').height( $('html').outerHeight() ).fadeTo(100, 0.5);
-    };
-
+  }
+  // Set all panel divs to default style, in case one is currently in focus
+  $('div.panel').css({
+    'z-index': '0',
+    'position': 'static',
+    '-webkit-box-shadow': 'none',
+    '-moz-box-shadow': 'none',
+    'box-shadow': 'none'
   });
-
-});})(jQuery);
+  // Bring the desired panel div into focus
+  $('div.panel#' + id).css({
+    'z-index': '999',
+    'position': 'relative',
+    '-webkit-box-shadow': '0px 0px 30px rgba(0, 0, 0, 0.32)',
+    '-moz-box-shadow': '0px 0px 30px rgba(0, 0, 0, 0.32)',
+    'box-shadow': '0px 0px 30px rgba(0, 0, 0, 0.32)'
+  });
+  // RELEASE THE UNDERLAY
+  if (!$('#modal-underlay').length) {
+    $('body').append('<div id="modal-underlay" onclick="closeBg(\''+id+'\');" style="  display: none; width: 100%; height: 100%; background: #000; position: fixed; top: 0; left: 0; z-index: 998;"></div>');
+    $('#modal-underlay').height( $('html').outerHeight() ).fadeTo(100, 0.5);
+  }
+}
 
 function closeBg(id) {
   $ = jQuery;
